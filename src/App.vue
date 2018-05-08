b4b3b60d8e08dae52e38391053eb444c0e2c014d
zalman sorotzkin <zsorotzk@g.jct.ac.il>
2018-05-08 21:42:16 +0300

    add dvora

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div id="app">
+      <v-app>
+      <v-content>
+        <my-component>
+      </v-content>
+    </v-app>
+  </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import myComponent from './components/myComponent.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    myComponent:myComponent
   }
 }
 </script>
---
+++ src/components/myComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-layout>
+      
+    <v-flex xs12 sm6 offset-sm3>
+           <v-card>
+          <v-card-title>
+              <div>
+            <h1 class="headline mb-0">דבורושקה אהובתי</h1>
+            <h1 class="headline mb-0">מוקדש באהבה</h1>
+          </div>
+          </v-card-title>
+        </v-card>
+      <v-card>
+       <a href="https://www.youtube.com/watch?v=p7zf_3q8bMg" width="700">
+        <v-card-media src='https://www.zimmer.co.il/hipo/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20455.jpg' height="200px">
+        </v-card-media></a>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">הליכה לקיסריה</h3>
+            <div>....הרוח והחושך והמים</div>
+          </div>
+        </v-card-title>
+        <!-- <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions> -->
+      </v-card>
+    
+    </v-flex>
+    
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'myComponent',
+ 
+  props: {
+  }
+}
+</script>
